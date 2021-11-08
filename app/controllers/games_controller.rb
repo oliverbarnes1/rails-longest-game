29fb89d0038c2df258b20b00e071499cd0c7f025
Oliver Barnes <oliverbarnes124@gmail.com>
2021-11-08 16:22:12 +0000

    done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,38 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @letters = generate_grid(9)
+    @time = Time.now
   end
 
   def score
+    @time_taken = params[:time].to_i - Time.now.to_i
     @letters = params[:letters]
-    @score = params[:guess]
-    # check_answer(@score, @letters)
+    @guess = params[:guess].upcase
+    @result = included?(@guess, @letters)
+    @english = english_word?(@guess)
+    @score = compute_score(@guess, @time_taken)
   end
 
   def generate_grid(grid_size)
     Array.new(grid_size) { ('A'..'Z').to_a.sample }
   end
 
-  # def check_answer(score, letters)
+  def included?(guess, letters)
+    guess.chars.all? { |letter| guess.count(letter) <= letters.count(letter) }
+  end
 
-  # end
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+  def compute_score(attempt, time_taken)
+    time_taken > 60.0 ? 0 : attempt.size * ((1.0 - time_taken / 60.0) / 100_000).round
+  end
+
+
 end
